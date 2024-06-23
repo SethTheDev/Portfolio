76f1a2db120a2fff47c8d2fea25bddc2b48164f8
SethPython <spccmbseth@gmail.com>
2024-06-23 20:55:04 +0530

    Update Review Animation

--- src/app/components/review.tsx
+++ src/app/components/review.tsx
@@ -1,7 +1,8 @@
 "use client"
 
-import React, { useRef, useEffect } from "react"
-import { animate } from "motion"
+import React, { useEffect } from "react"
+import { useInView } from "react-intersection-observer"
+import { motion, useAnimation } from "framer-motion"
 
 interface ReviewParams {
     avatar: string,
@@ -9,36 +10,41 @@ interface ReviewParams {
     description: string
 }
 
+const variants = {
+    visible: { opacity: 1, transition: { duration: 1 } },
+    hidden: { opacity: 0 },
+    hovered: { scale: 1.2, rotate: 10, transition: { duration: 0.2 } },
+    not_hovered: { scale: 1, rotate: 0, transition: { duration: 0.2 } }
+}
+
 export function Review({ avatar, name, description }: ReviewParams) {
-    const ref = useRef<HTMLDivElement>(null)
+    const control = useAnimation()
+    const [ref, inView] = useInView()
+
+    const handleMouseEnter = () => {
+        control.start("hovered")
+    }
+
+    const handleMouseLeave = () => {
+        control.start("not_hovered")
+    }
 
     useEffect(() => {
-        const element = ref.current
-
-        const handleMouseEnter = () => {
-            animate(element as Element, { scale: 1.2, rotate: 10 })
+        if (inView) {
+            control.start("visible")
         }
-
-        const handleMouseLeave = () => {
-            animate(element as Element, { scale: 1, rotate: 0 })
-        }
-
-        const handleClick = () => {
-            alert('btw it\'s πr²')
-            element?.removeEventListener('mousedown', handleClick)
-        }
-
-        if(element) {
-            element.addEventListener('mouseenter', handleMouseEnter);
-            element.addEventListener('mouseleave', handleMouseLeave);
-            if (name === 'Nitlix') {
-                element.addEventListener('mousedown', handleClick)
-            }
-        }
-    })
+    }, [control, inView])
 
     return (
-        <div ref={ref} className="review">
+        <motion.div
+         ref={ref}
+         className="review" 
+         onMouseEnter={handleMouseEnter} 
+         onMouseLeave={handleMouseLeave}
+         variants={variants}
+         initial="hidden"
+         animate={control}
+         >
             <div className="flex flex-row flex-wrap items-center gap-5">
                 <img className="avatar" src={avatar} width="64" height="64"></img>
                 <h2 className="text-2xl">{name}</h2>
@@ -51,6 +57,6 @@ export function Review({ avatar, name, description }: ReviewParams) {
                     </React.Fragment>
                 ))}
             </p>
-        </div>
+        </motion.div>
     )
 }
--- src/app/components/terminal.tsx
+++ src/app/components/terminal.tsx
@@ -10,9 +10,9 @@ const nunitoTitle = Nunito({ weight: "800", subsets: ["latin"] })
 export function Terminal() {
     return (
         <div className={`terminal ${nunito.className}`}>
-            <div className="flex flex-row items-center justify-between">
-                <strong className={`${nunitoTitle.className} flex-grow text-center`}>kitty - usr/bin/zsh</strong>
-                <strong className={`${nunitoTitle.className}`}><i className="not-italic text-lime-400">&#95;</i> <i className="not-italic text-yellow-500">&#x25A2;</i> <i className="not-italic text-rose-700">&times;</i></strong>
+            <div className={`${nunitoTitle.className} flex flex-row items-center justify-between`}>
+                <strong className="flex-grow text-center">kitty - usr/bin/zsh</strong>
+                <strong><i className="not-italic text-lime-400">&#95;</i> <i className="not-italic text-yellow-500">&#x25A2;</i> <i className="not-italic text-rose-700">&times;</i></strong>
             </div>
             <div className="flex flex-row flex-wrap">
                 <strong className={nunitoTitle.className}>~ /&gt;&emsp;</strong>
