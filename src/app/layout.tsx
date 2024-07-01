ac2202de66ef102cbba6b27881ac2a132ceebea6
SethPython <spccmbseth@gmail.com>
2024-07-01 23:44:53 +0530

    Update Embed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,15 @@ import "./globals.css";
 const poppins = Poppins({ weight: "300",  subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Home | ThePython",
-  description: "Empowering the future",
+  openGraph: {
+    title: 'SethPython',
+    description: 'Your average fullstack developer',
+    images: ['/img/logo/logo_full.png'],
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image'
+  }
 };
 
 export default function RootLayout({
