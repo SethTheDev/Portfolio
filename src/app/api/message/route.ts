c4a349c21134aa89c94393f55a25fe1f0658c8a6
SethPython <spccmbseth@gmail.com>
2024-07-14 00:23:43 +0530

    fix again

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -21,7 +21,7 @@ export async function POST(
                 },
                 body: JSON.stringify({
                     username: name,
-                    content: `**Message from \`${name}\`:** \`${message.replaceAll('\\', '')}\``
+                    content: `**Message from \`${name}\`:** \`${message.replaceAll('\\', '').replaceAll('`', '')}\``
                 })
             }
         )
