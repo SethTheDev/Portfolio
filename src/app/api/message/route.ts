c457bfe304d41688f5e3f817fd84a4f4046f4096
SethPython <spccmbseth@gmail.com>
2024-07-13 21:18:37 +0530

    uwu

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -1,14 +1,13 @@
-import type { NextApiRequest } from "next";
 import { Webhook } from "@ly-nld/dishook";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 const hook = new Webhook(process.env.WEBHOOK_URL as string)
 
 export async function POST(
-    req: NextApiRequest
+    req: NextRequest
 ) {
     try {
-        const { name, message } = req.body
+        const { name, message } = await req.json()
         hook.setUsername(name).setContent(`Hey <@866510980225040466>, ${name} says ${message}`).send()
         return NextResponse.json({
             success: true
